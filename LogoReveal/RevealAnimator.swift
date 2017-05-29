55d3db09b415440d18e15ba51a9f0ba91a5a5a92
ElijahButers <Elijahbuters@gmail.com>
2017-05-29 23:58:14 +0300

    Added a new cocoa touch class, a subclass of NSObject to the project.

---
+++ LogoReveal/RevealAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  RevealAnimator.swift
+//  LogoReveal
+//
+//  Created by User on 5/29/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class RevealAnimator: NSObject {
+
+}
