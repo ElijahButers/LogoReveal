c10242006d15fa4e395b3257803f9e58fd67b7db
ElijahButers <Elijahbuters@gmail.com>
2017-06-03 21:29:48 +0300

    Added code to the animateTransition() in the RevealAnimator class that grows the logo 150 times in size and moves it up a little at the same time.

--- LogoReveal/RevealAnimator.swift
+++ LogoReveal/RevealAnimator.swift
@@ -25,6 +25,10 @@ class RevealAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         let toVC = transitionContext.viewController(forKey: .to) as! DetailViewController
         transitionContext.containerView.addSubview(toVC.view)
         toVC.view.frame = transitionContext.finalFrame(for: toVC)
+        
+        let animation = CABasicAnimation(keyPath: "transform")
+        animation.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+        animation.toValue = NSValue(caTransform3D: CATransform3DConcat(CATransform3DMakeTranslation(0.0, -10.0, 0.0), CATransform3DMakeScale(150.0, 150.0, 1.0)))
     }
 
 }
